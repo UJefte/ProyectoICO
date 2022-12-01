50e55dcb403d6ed198193b572a47d46ded8858b7
UJefte <uri.aguirre05@gmail.com>
2022-11-30 18:32:53 -0600

    Inicializamos nuestros arreglos y base al numero de graods que tenga el polinomio pediremos el valor de cada uno de sus grados, tanto del uno como del dos

--- ProyectoIntroICO.cpp
+++ ProyectoIntroICO.cpp
@@ -10,4 +10,15 @@ int main() {
 	grado1 = pedir_grado();
 	printf("\nPolinomio 2: ");
 	grado2 = pedir_grado();
+	float Polinomio1[grado1 + 1];
+	float Polinomio2[grado2 + 1];
+	printf("\nLlenando el polinomio 1: ");
+	ingresarValo(Polinomio1, grado1);
+	printf("\nLlenando el polinomio 2: ");
+	ingresarValo(Polinomio2, grado2);
+	printf("\nPolinomio 1: ");
+	mostrarPoli(Polinomio1, grado1);
+	printf("\nPolinomio 2: ");
+	mostrarPoli(Polinomio2, grado2);
+	return 0;
 }
