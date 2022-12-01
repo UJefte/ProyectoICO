5d05dcfa9ebb5a69d4a208bac3b2296168cb36ad
UJefte <uri.aguirre05@gmail.com>
2022-11-30 18:29:32 -0600

    Comenzamos con la estructura del programa, declaramos dos variables de tipo entero y pediremos que nos den los grados a los que esta elevado los polinomios

--- ProyectoIntroICO.cpp
+++ ProyectoIntroICO.cpp
@@ -3,3 +3,11 @@ int pedir_grado();
 void ingresarValo(float* Polinomio, int grado);
 void sumaPolinomio(float* Polinomio1, float* polinomio2, int grado1, int grado2);
 void mostrarPoli(float* Polinomio, int grado);
+int main() {
+	int grado1;
+	int grado2;
+	printf("Polinomio 1: ");
+	grado1 = pedir_grado();
+	printf("\nPolinomio 2: ");
+	grado2 = pedir_grado();
+}
