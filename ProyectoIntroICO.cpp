61e49aba249f3838402e1d4042b7796bd2715b2c
UJefte <uri.aguirre05@gmail.com>
2022-11-30 18:16:48 -0600

    Creamos tres funciones una para pedir el grado, otras para ingresar el valor y la otras para mostrar los polinomios

---
+++ ProyectoIntroICO.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int pedir_grado();
+void ingresarValo(float* Polinomio, int grado);
+void sumaPolinomio(float* Polinomio1, float* polinomio2, int grado1, int grado2);
+void mostrarPoli(float* Polinomio, int grado);
